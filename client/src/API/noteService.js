5c601db277c23b27cf33d2bca37f43f5357099ad
Your Name <bekzot.abdyldayev@gmail.com>
2022-06-12 19:24:34 +0600

    src/API/noteService.js URL updated

--- client/src/API/noteService.js
+++ client/src/API/noteService.js
@@ -1,26 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 
 class NoteService {
   async getAll() {
-    const response = await axios.get("http://localhost:4000/notes/all");
+    const response = await axios.get(
+      'https://notes-g3pka4jpo-unix238.vercel.app/notes/all'
+    );
     return response.data;
   }
 
   async create(note) {
-    const response = await axios.post("http://localhost:4000/notes/add", note);
+    const response = await axios.post(
+      'https://notes-g3pka4jpo-unix238.vercel.app/notes/add',
+      note
+    );
     console.log(response);
   }
 
   async delete(note) {
     const response = await axios.delete(
-      "http://localhost:4000/notes/delete/" + note._id
+      'https://notes-g3pka4jpo-unix238.vercel.app/notes/delete/' + note._id
     );
     console.log(response);
   }
 
   async update(note) {
     const response = await axios.put(
-      "http://localhost:4000/notes/update/" + note._id,
+      'https://notes-g3pka4jpo-unix238.vercel.app/notes/update/' + note._id,
       note
     );
     console.log(response);
